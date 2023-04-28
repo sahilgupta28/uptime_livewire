7096b5799849e566a21371c9bf5e45ebe9d122e9
Sahil Gupta <sahilgupta@founderandlightning.com>
2023-04-28 13:50:02 +0530

    Write liveeire TDD

---
+++ tests/Feature/ProjectTest.php
@@ -0,0 +1,50 @@
+<?php
+
+use App\Models\User;
+
+test('project create page is displayed', function () {
+    $user = User::factory()->create();
+
+    $response = $this->actingAs($user)->get(route('project.create'));
+
+    $response->assertOk();
+});
+
+test('project index page is displayed', function () {
+    $user = User::factory()->create();
+
+    $response = $this->actingAs($user)->get(route('project.index'));
+
+    $response->assertOk();
+});
+
+test('project create form save', function () {
+    Livewire::test('create-project-form')
+        ->set('project_name', '')
+        ->set('url', '')
+        ->assertHasErrors([
+            'project_name',
+            'url'
+        ]);
+
+    Livewire::test('create-project-form')
+        ->assertOk();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
